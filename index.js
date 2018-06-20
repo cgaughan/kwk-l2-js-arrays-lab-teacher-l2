620c63f6ed457b6e28cbd1aaf313c81b336f3f48
Caroline Gaughan <cgaughan1122@gmail.com>
2018-06-20 02:32:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const drivers = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendDriver(name) {
   drivers.push("Ralph");
-  return drivers;
 }
 
 function destructivelyPrependDriver() {
   drivers.unshift("Bob");
-  //return drivers;
 }
